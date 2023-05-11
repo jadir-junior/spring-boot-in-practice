0d49768b2e233d05f6bf3e8718091b07e0b29a0d
Jadir Junior <jadirjsjunior@gmail.com>
2023-05-11 11:11:12 -0300

    Configuring a relational database in Spring Boot application

--- src/test/java/com/jj/springbootinpractice/SpringBootInPracticeApplicationTests.java
+++ src/test/java/com/jj/springbootinpractice/SpringBootInPracticeApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jj.springbootinpractice;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class SpringBootInPracticeApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void givenDatasourceAvailableWhenAccesssDetailsTheExpectDetails() throws SQLException {
+		assertThat(dataSource.getClass().getName()).isEqualTo("com.zaxxer.hikari.HikariDataSource");
+		assertThat(dataSource.getConnection().getMetaData().getDatabaseProductName()).isEqualTo("H2");
 	}
 
 }
